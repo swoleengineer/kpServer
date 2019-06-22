59f244446a89820900443036c086b100b8707f22
Joram Clervius <clervius@gmail.com>
2019-06-21 19:19:56 -0600

    Cannot get connection to work.

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = config => {
-  mongoose.connect(config.db, { dbName: 'keenpages001'});
+  mongoose.connect(config.db, { dbName: 'keenpages001', useMongoClient: true, useNewUrlParser: true });
   mongoose.Promise = global.Promise;
 
   const db = mongoose.connection;
